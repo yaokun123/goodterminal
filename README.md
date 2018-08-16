7eb036e96cd3135434b96b7342bec73019e0aa29
System Administrator <root@yaokdeMac-mini.local>
2018-08-16 11:32:58 +0800

    add new file

---
+++ README.md
@@ -0,0 +1,20 @@
+goodterminal
+============
+
+打造一个漂亮的终端包括git
+
+
+用法
+
+cd /etc/
+
+
+sudo git clone https://github.com/suxianbaozi/goodterminal.git
+
+sudo vi /etc/bashrc 
+加入内容 
+source /etc/goodterminal/gtprofile
+
+立即生效的话，直接执行命令
+
+source /etc/goodterminal/gtprofile
---
+++ color_show.bash
@@ -0,0 +1,31 @@
+
+force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+    # We have color support; assume it's compliant with Ecma-48
+    # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+    # a case would tend to support setf rather than setaf.)
+    color_prompt=yes
+    else
+    color_prompt=
+    fi
+fi
+
+black=$'\[\e[1;30m\]'
+red=$'\[\e[1;31m\]'
+green=$'\[\e[1;32m\]'
+yellow=$'\[\e[1;33m\]'
+blue=$'\[\e[1;34m\]'
+magenta=$'\[\e[1;35m\]'
+cyan=$'\[\e[1;36m\]'
+white=$'\[\e[1;37m\]'
+normal=$'\[\e[m\]'
+
+
+if [ "$color_prompt" = yes ]; then
+    PS1="$blue[$magenta\u$blue@$green\h$blue:$cyan\w$red]\$ $normal"
+    #PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w$(__git_ps1 " (%s)")\$'
+fi
